602740d0293d582a9d8d3fa5d777e3a01a89bbb3
AVICCII <863134471@qq.com>
2020-10-29 17:35:01 +0800

    2020/10/29

---
+++ src/JVM/Demo.java
@@ -0,0 +1,14 @@
+package JVM;
+
+/**
+ * @author aviccii 2020/10/29
+ * @Discrimination
+ */
+public class Demo {
+    public static void main(String[] args) {
+        new Thread(()->{
+
+        },"my thread name").start();
+
+    }
+}
