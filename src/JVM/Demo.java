2cef0ed7208a9be36ab95825701cd807134058b7
AVICCII <863134471@qq.com>
2020-11-05 17:22:30 +0800

    2020/11/5

--- src/JVM/Demo.java
+++ src/JVM/Demo.java
@@ -9,6 +9,18 @@ public class Demo {
         new Thread(()->{
 
         },"my thread name").start();
-
     }
+
+    //native:凡是带了native关键字的，说明java的作用范围达不到了，回去调用底层C语言的库
+    //会进入本地方法栈
+    //调用本地方法接口 JNI
+    //JNI作用:扩展java的使用，融合不同的编程语言为java所用
+    //java诞生的时候 C和C++盛行 所以必须调用他们
+    //它在内存区域中专门开辟了一块标记区域：native Method Stack，登记native方法
+    //在最终执行的时候，加载本地方法库中的方法通过JNI
+
+    //java程序驱动打印机，管理系统
+    private native void start0();
+
+    //调用其他接口：  Socket,WebService,Http
 }
